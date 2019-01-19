1490fb640957b5368716f37954b381450058028b
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-01-19 01:14:09 +0000

    Words from the array are displayed one after the other. Also added a cursor to the css and a fullstop in the html

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const TypeWriter = function (txtElement, words, wait = 3000) {
     this.wait = parseInt(wait, 10);
     this.type();
     this.isDeleting = false;
+    
 }
+
+let speed = 50;
 
 //Type method
 TypeWriter.prototype.type = function () {
@@ -38,10 +41,40 @@ TypeWriter.prototype.type = function () {
 
     //Insert text into element
 
-    this.txtElement.innerHTML = `<span class = ""txt>${this.txt}</span`;
+    this.txtElement.innerHTML = `<span class ="txt">${this.txt}</span`;
 
+    //Type Speed
+    let typeSpeed = speed;
 
-    setTimeout(() => this.type(), 500)
+    if(this.isDeleting){
+        typeSpeed /= 2;
+    }
+
+    // if word is complete
+
+    if(!this.isDeleting && this.txt === fullText){
+
+        //pauses at end
+        typeSpeed = this.wait;
+
+        //set delete to true
+
+        this.isDeleting = true;
+    }
+    else if(this.isDeleting && this.txt === ''){
+        
+        this.isDeleting = false;
+        //Move to next word
+        this.wordIndex++;
+
+        //Pause before starting to type
+
+        typeSpeed = speed;
+
+        
+    }
+
+    setTimeout(() => this.type(),typeSpeed)
 }
 
 
